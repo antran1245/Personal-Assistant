9ee38b67cec685d27df92dcb20c763bc5efedb44
antran1245 <antran1245@gmail.com>
2023-02-12 11:04:26 -0800

    set up for audio to text

--- index.py
+++ index.py
@@ -1,1 +1,28 @@
-print('hello world')
+import speech_recognition as sr
+
+
+def printDevice():
+    for i in range(0, len(sr.Microphone.list_microphone_names())):
+        if "Microphone" in sr.Microphone.list_microphone_names()[i]:
+            print(i, sr.Microphone.list_microphone_names()[i])
+
+
+def micToText():
+    with sr.Microphone(device_index=1) as source:
+        print('Talk')
+        audio_text = sr.Recognizer().listen(source)
+        print("Time to talk is over.")
+
+    recognize_text = ""
+    try:
+        recognize_text = sr.Recognizer().recognize_google(audio_text)
+        print("Text: "+recognize_text)
+    except:
+        print("Could not recognize voice input")
+
+    print("Final result " + recognize_text)
+
+
+if __name__ == "__main__":
+    # printDevice()
+    micToText()
